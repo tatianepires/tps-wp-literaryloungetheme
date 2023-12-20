a2de7f761a8abf5657db0f718fb6bedd139fb7a2
Tatiane Pires <tatiane@tatianepires.net>
2023-12-20 16:54:01 -0300

    Add favicon

--- inc/frontend-scripts.php
+++ inc/frontend-scripts.php
@@ -5,6 +5,7 @@
  * Enqueue theme's CSS and JS
  */
 
+add_action( 'wp_enqueue_scripts', 'llp_frontend_scripts' );
 function llp_frontend_scripts(): void {
   // Parent theme's CSS
   wp_enqueue_style('twentytwentyfour', get_template_directory_uri() . '/style.css' );
@@ -23,4 +24,8 @@ function llp_frontend_scripts(): void {
 	// Theme's CSS
 	wp_enqueue_style('llp', get_stylesheet_directory_uri() . '/css/styles.min.css', array('twentytwentyfour'));
 }
-add_action( 'wp_enqueue_scripts', 'llp_frontend_scripts' );
+
+add_action( 'wp_head', 'llp_favicon');
+function llp_favicon() {
+	printf('<link rel="shortcut icon" type="image/jpg" href="%s/favicon.ico"/>', get_stylesheet_directory_uri(), );
+}
